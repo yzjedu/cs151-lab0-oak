e4cbb8c6c9fb2619d83c247fecd73d6d24b5241d
kpinkins <kpinkins@loyola.edu>
2024-09-05 13:22:11 -0400

    Initial Commit

---
+++ Lab0.py
@@ -0,0 +1,8 @@
+print("CS151 Lab 0")
+print("Eric")
+print("Eric doesn't like fish")
+print("Eric lives in Baltimore")
+print("Eric likes Red")
+print("Eric has two sisters")
+print("Eric has a brother")
+
