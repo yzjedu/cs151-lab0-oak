d7b5bedad20dc381fd8ea515ffb7fe618bcf009d
EricThomas28 <emthomas1@loyola.edu>
2024-09-10 13:47:18 -0400

    #programer: Eric and Krishon #course: CS151 Dr. Isaacman #Due date: 9/12/24 #Lab assignment: 0 #Problem statement: output information about the programmers #Data in: None, there is no user input in this program #Data out: The names of the programmers and five statements about each of them #Credits: Dr. Isaacman
    print("Krishon")
    print("Krishon likes fish")
    print("Kirshon is from Chicago")
    print("Krishon has two younger sister")
    print("Krishon has one younger brother")
    print("Krishon favorite food is french fries")

--- Lab0.py
+++ Lab0.py
@@ -1,3 +1,12 @@
+#programer: Eric and Krishon
+#course: CS151 Dr. Isaacman
+#Due date: 9/12/24
+#Lab assignment: 0
+#Problem statement: output information about the programmers
+#Data in: None, there is no user input in this program
+#Data out: The names of the programmers and five statements about each of them
+#Credits: Dr. Isaacman
+
 print("CS151 Lab 0")
 print("Eric")
 print("Eric doesn't like fish")
@@ -5,4 +14,14 @@ print("Eric lives in Baltimore")
 print("Eric likes Red")
 print("Eric has two sisters")
 print("Eric has a brother")
+
+print("Krishon")
+print("Krishon likes fish")
+print("Kirshon is from Chicago")
+print("Krishon has two younger sister")
+print("Krishon has one younger brother")
+print("Krishon favorite food is french fries")
+
+
+
 
